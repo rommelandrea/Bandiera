b6cda0d4e0ba7234cb4def9eaf6f23cc0af244f8
Andrea Romanello <rommelandrea@gmail.com>
2011-10-17 15:19:36 +0200

    creata funzione ordinaColore create funzioni di supporto swap e print

--- bandiere.c
+++ bandiere.c
@@ -14,6 +14,7 @@
 int ordinatoColori(int [], int);
 void ordinaColori(int [], int);
 void riempiArrayCasuale(int [], int, int);
+void swap(int [], int, int);
 
 void riempiArrayCasuale(int a[], int length, int max){
     srand(time(NULL));
@@ -33,7 +34,58 @@ int ordinatoColori(int a[], int length){
     return 0;
 }
 
+void ordinaColori(int a[], int length){
+    int i, j, k;
+    
+    i = j = 0;
+    k = length-1;
+    
+    while(j<=k){
+        if(a[j]%3==0){
+            swap(a, i, j);
+            i++;
+            j++;
+        }
+        if(a[j]%3==1 && j<=k){
+            j++;
+        }
+        if(a[j]%3==2 && j<=k){
+            swap(a, j, k);
+            k--;
+        }
+    }
+}
+
+void swap(int a[], int i, int j){
+    int temp;
+    temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
+
+void print(int a[], int length){
+    int i;
+    printf("stampo l'array\n");
+    for(i=0; i<length; i++){
+        printf("%d-->", a[i]);
+    }
+    printf("\n");
+}
+
 int main(int argc, char** argv) {
+    int length = 10;
+    int max = 3;
+    int boolan;
+    
+    int a[length];
+    
+    riempiArrayCasuale(a, length, max);
+    
+    print(a, length);
+    
+    ordinaColori(a, length);
+    
+    print(a, length);
     
     return (EXIT_SUCCESS);
 }
