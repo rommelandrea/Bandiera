59842c7b6c6b4525eb0bbacaacdc341c2e37bc3a
Andrea Romanello <rommelandrea@gmail.com>
2011-10-17 13:19:46 +0200

    creati prototipi di funzione creato riempi array random

--- bandiere.c
+++ bandiere.c
@@ -1,18 +1,30 @@
 /* 
  * File:   bandiere.c
- * Author: rommel
+ * Author: Andrea Romanello
  *
- * Created on October 17, 2011, 1:11 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /*
  * 
  */
-int main(int argc, char** argv) {
+int ordinatoColori(int [], int);
+void ordinaColori(int [], int);
+void riempiArrayCasuale(int [], int, int);
 
+void riempiArrayCasuale(int a[], int length, int max){
+    srand(time(NULL));
+    int i;
+    
+    for(i=0; i<length; i++)
+        a[i] = rand()%max;
+}
+
+int main(int argc, char** argv) {
+    
     return (EXIT_SUCCESS);
 }
 
