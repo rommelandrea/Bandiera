28c2070ac2d78b8803d32e2ce7e2c78399db0489
Andrea Romanello <rommelandrea@gmail.com>
2011-10-17 13:13:07 +0200

    Inizializzo il progetto

---
+++ bandiere.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   bandiere.c
+ * Author: rommel
+ *
+ * Created on October 17, 2011, 1:11 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
